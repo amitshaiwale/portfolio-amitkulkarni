e57cb7b7a82423e36ffcd09401a718d2979315ba
amitshaiwale <amitshaiwale20@gmail.com>
2021-09-11 12:32:14 +0530

    mobile view, firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCFoHvwRMhk0ZbVtmKC8ocwN0SytmuE930",
+    authDomain: "portfolio-amitkulkarni.firebaseapp.com",
+    projectId: "portfolio-amitkulkarni",
+    storageBucket: "portfolio-amitkulkarni.appspot.com",
+    messagingSenderId: "216884516921",
+    appId: "1:216884516921:web:2a8ba1f2c05e2358b015e8",
+    measurementId: "G-1ZQH4WWV9Q"
+  }
 };
